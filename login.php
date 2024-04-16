dcc43a13b664aa127052f3c460f4cff89ba77e7a
aldoaquino451 <aldoaquino2909@gmail.com>
2024-04-16 18:24:18 +0200

    [add] login with validation

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.css' integrity='sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==' crossorigin='anonymous' />
+  <title>PHP Blog</title>
+</head>
+
+<body>
+
+  <div style="max-width: 60%; margin: 0 auto;" class="my-5 container">
+    <h1 class="text-center">Login</h1>
+
+    <form action="function.php" method="post">
+      <div class=" mb-3">
+        <label for="user" class="form-label">User</label>
+        <input type="text" class="form-control" id="user" name="user" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" name="password">
+      </div>
+      <button submit="saluto()" class="btn btn-primary">Invia</button>
+    </form>
+  </div>
+
+</body>
+
+</html>
