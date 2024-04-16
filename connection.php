a8d3abbb42f2622c68f11ec81f5069391c10a6db
aldoaquino451 <aldoaquino2909@gmail.com>
2024-04-16 18:23:24 +0200

    [edit] connection class

--- connection.php
+++ connection.php
@@ -1,56 +1,64 @@
 <?php
-// try {
-//   $pdo = new PDO("mysql:host=localhost;dbname=db_php_blog", "user", "user");
-//   // Set the PDO error mode to exception
-//   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//   die("ERRORE: Impossibile stabilire una connessione al database");
-// }
 
+class Database
+{
+  private $pdo;
 
-try {
-  $pdo = new PDO(
-    "mysql:host=localhost;dbname=db_php_blog",
-    "root",
-    "root",
-    [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"] // La connessione utilizzerà la codifica UTF-8
-  );
-  // Set the PDO error mode to exception
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-  // die("Impossibile stabilire una connessione al database: " . $e.getMessage());
-  die("Impossibile stabilire una connessione al database: ");
+  /*
+    Stabilisco la connessione
+  */
+  public function make_connection()
+  {
+    try {
+      $this->pdo = new PDO(
+        "mysql:host=localhost;dbname=db_php_blog",
+        "root",
+        "root",
+        [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]
+      );
+      $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+      // die("Impossibile stabilire una connessione al database: " . $e.getMessage());
+      die("Impossibile stabilire una connessione al database: ");
+    }
+  }
+
+  /*
+    Creo una nuova tabella
+  */
+  public function make_table()
+  {
+    $sql = "CREATE TABLE `db_php_blog`.`prodotti` (
+      `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+      `nome` VARCHAR(300) NOT NULL,
+      `prezzo` DECIMAL(6,2) NOT NULL,
+      PRIMARY KEY (`id`) ) ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;";
+
+    $this->pdo->exec($sql);
+  }
+
+  /*
+    Inserisco dei dati
+  */
+  public function add_product()
+  {
+    $sql_data = 'INSERT INTO prodotti (nome, prezzo) VALUES ("sapone liquido 100ml", 8.50)';
+
+    $this->pdo->exec($sql_data);
+  }
+
+  public function select_product()
+  {
+    $sql = 'SELECT username, password FROM users';
+    $sth = $this->pdo->prepare($sql);
+    $result = $sth->execute();
+
+    if (!$result) {
+      die('Errore esecuzione query: ' . implode(',', $this->pdo->errorInfo()));
+    }
+
+    $products = $sth->fetchAll(PDO::FETCH_OBJ);
+
+    return $products;
+  }
 }
-
-
-$sql = "CREATE TABLE `db_php_blog`.`prodotti` (
-  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(300) NOT NULL,
-  `prezzo` DECIMAL(6,2) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;";
-
-$result = $pdo->exec($sql);
-
-if ($result) {
-  echo "Tabella creata con successo";
-}
-
-$sql_data = 'INSERT INTO prodotti (nome, prezzo) VALUES ("sapone liquido 100ml", 8.50)';
-
-$affectedRows = $pdo->exec($sql_data);
-
-echo $affectedRows;
-/*
-  Stabilisco la connessione
-*/
-
-/*
-  Creo una nuova tabella
-*/
-
-
-
-/*
-  Inserisco dei dati
-*/
